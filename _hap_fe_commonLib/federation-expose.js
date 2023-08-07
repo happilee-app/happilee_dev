c114efad28f1fea3b3f639df91061f9d892f39f6
sudhi-neoito <sudhi@neoito.com>
2023-08-07 16:09:42 +0530

    test

---
+++ _hap_fe_commonLib/federation-expose.js
@@ -0,0 +1,3 @@
+export default {
+    "./_hap_fe_commonLib": "./src/remote/_hap_fe_commonLib",
+}
---
+++ _hap_fe_commonLib/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import _hap_fe_commonlib from './remote/_hap_fe_commonLib'
+
+export default function App() {
+  return (
+    <div className="App" data-testid="app">
+      <_hap_fe_commonlib />
+    </div>
+  )
+}
