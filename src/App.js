e4c35147f227b7ba4f9c25e202fa4d48c74effea
Vishnu Raj <vishnurajcl@gmail.com>
2023-08-15 10:30:38 +0530

    [commitHash:b0a9417]

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import _hap_fe_firstuielements from './remote/_hap_fe_firstUiElements'
+
+export default function App() {
+  return (
+    <div className="App" data-testid="app">
+    <_hap_fe_firstuielements/>
+    </div>
+  )
+}
---
+++ src/remote/_hap_fe_firstUiElements.js
@@ -0,0 +1,9 @@
+
+import React from 'react'
+
+export const _hap_fe_firstuielements = () => {
+  return (
+   <p>Hello from _hap_fe_firstUiElements </p>
+  )
+}
+export default _hap_fe_firstuielements
